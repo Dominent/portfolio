23aa13d7cf59b72adbe7856653862af1815eb426
Dominent <petromilpavlov@gmail.com>
2018-07-03 01:16:27 +0300

    Clean Unused Dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -14,24 +14,13 @@ import setAuthToken from './utils/setAuthToken';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/create-profile/CreateProfile';
 
 import './App.css';
-import EditProfile from './components/edit-profile/EditProfile';
-import AddExperience from './components/add-credentials/AddExperience';
-import AddEducation from './components/add-credentials/AddEducation';
-import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile';
-import NotFound from './components/not-found/NotFound';
-import Posts from './components/posts/Posts';
 import Gallery from './components/gallery/Gallery';
 import Contacts from './components/contacts/Contacts';
-import Wrapper from './components/gallery/Wrapper';
 import GalleryDetails from './components/gallery/GalleryDetails';
-import { AnimatedRoute } from 'react-router-transition';
 
 if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
@@ -51,23 +40,6 @@ if (localStorage.jwtToken) {
   }
 }
 
-const slideInAnimation = {
-  atEnter: {
-    opacity: 0,
-    left: 100,
-    backgroundColor: '#fff'
-  },
-  atLeave: {
-    opacity: 0,
-    left: 100,
-    backgroundColor: '#fff'
-  },
-  atActive: {
-    opacity: 1,
-    left: 0
-  }
-}
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -79,10 +51,10 @@ class App extends Component {
           <div className="App">
             <Route exact path="/gallery" component={GalleryDetails} />
             <Route exact path="/" component={Spa} />
-            
+
             <Route exact path="/login" component={Login} />
             <Switch>
-                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </div>
         </Router>
@@ -93,7 +65,7 @@ class App extends Component {
 
 const Spa = () =>
   (
-    <Wrapper>
+    <React.Fragment>
       <Navbar />
       <Landing />
 
@@ -106,37 +78,7 @@ const Spa = () =>
       </div>
 
       <Footer />
-    </Wrapper>
+    </React.Fragment>
   )
 
 export default App;
-
-/*
- <Route exact path="/" component={Landing} />
-            <div className="container">
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/profiles" component={Profiles} />
-              <Route exact path="/profile/:handle" component={Profile} />
-              <Route exact path="/gallery" component={Gallery} />
-              <Switch>
-                <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path="/edit-profile" component={EditProfile} />
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path="/add-experience" component={AddExperience} />
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path="/add-education" component={AddEducation} />
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path="/feed" component={Posts} />
-              </Switch>
-              <Route exact path="/not-found" component={NotFound} />
-            </div>
-*/
