ef1a34903c07fa9e1b48f66a328f093bfa655ccd
Dominent <petromilpavlov@gmail.com>
2018-07-25 22:59:42 +0300

    Prep for heroku deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require('passport');
+const path = require('path');
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -39,6 +40,12 @@ app.use("/api/contacts", contacts);
 
 // app.use("/api/projects", projects);
 // app.use("/api/files", files);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  
+  app.get('*', (req, res) =>  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const port = process.env.PORT || 5000;
 
