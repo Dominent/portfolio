23124b0c745ef72d64d212240016b21bcf1c6e93
Petromil Pavlov <petromilpavlov@gmail.com>
2018-08-04 22:30:06 +0300

    Create Basic Style Component, Early Draft

---
+++ client/src/components/common/Stylez.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+class Stylez extends Component {
+    createStyleElement = (css, component) => {
+        return <style id={component ? component : null} type="text/css" ref={(style) => this._style = style}
+            dangerouslySetInnerHTML={{  __html: css || ''  }} />
+    }
+
+    componentDidMount() {
+        const { component } = this.props;
+
+        let ownStyle = [...document.querySelectorAll(`#${component}`)]
+            .filter(x => !x.isEqualNode(this._style))[0];
+
+        if(!ownStyle) {
+            return;
+        }
+
+        let otherStyles = [...document.querySelectorAll(`#${component}`)]
+            .filter(x => x.isEqualNode(this._style));
+
+        if(!otherStyles.length) {
+            return;
+        }
+        
+        let css = otherStyles
+            .map(x => x.innerHTML)
+            .join('\n');
+
+        ownStyle.innerHTML += css;
+
+        otherStyles.forEach(el => el.remove());
+    }
+
+    render() {
+        const { children, css, component } = this.props;
+
+        return (<React.Fragment >
+            {  this.createStyleElement(css, component)  }
+            {children}
+        </React.Fragment>)
+    }
+}
+
+export default {
+    apply: (component, css, children) => {
+        return <Stylez css={css} component={component}>{children}</Stylez> 
+    }
+};
+
