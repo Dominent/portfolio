220551ad7bb3f6cd6832d119f6a7c5d4ed628b1d
Dominent <petromilpavlov@gmail.com>
2018-08-25 20:46:05 +0300

    Refactor Retry Mechanism

--- routes/api/proposals.js
+++ routes/api/proposals.js
@@ -26,27 +26,23 @@ router.post('/', (req, res) => {
      * When email service is called and the host is in sleep mode the service returns 503 for   no reason.
      * To fix this, if 503 is returned we call the email service 1 more time.
      */
-    const sendHtmlEmail = (data, retryCounter, maxRetries) => {
-        return emailService.sentHtmlAsync(data)
+    const MAX_RETRIES = 1;
+
+    for (let i = 0; i < MAX_RETRIES; ++i) {
+        emailService.sentHtmlAsync(data)
             .then(() => {
-                res.status(200).json({ message: 'success' });
+                return res.status(200).json({ message: 'success' });
             })
             .catch((err) => {
                 const SERVICE_UNAVAILABLE = 503;
 
                 if (err.response.status === SERVICE_UNAVAILABLE) {
-                    if (retryCounter < maxRetries) {
-                        return sendHtmlEmail(data, 1, maxRetries);
-                    }
+                    continue
                 }
 
-                res.status(400).json({ error: err });
+                return res.status(400).json({ error: err });
             });
     }
-
-    const MAX_RETRIES = 1;
-
-    sendHtmlEmail(data, 0, MAX_RETRIES);
 })
 
 module.exports = router;
