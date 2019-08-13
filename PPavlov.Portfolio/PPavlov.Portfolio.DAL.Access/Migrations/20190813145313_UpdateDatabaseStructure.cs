edd23ce5418dd118c232e74c2be7ada9e58aa5b6
ppavlov <petromilpavlov@gmail.com>
2019-08-13 17:55:02 +0300

    Updated Database

---
+++ PPavlov.Portfolio/PPavlov.Portfolio.DAL.Access/Migrations/20190813145313_UpdateDatabaseStructure.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PPavlov.Portfolio.DAL.Access.Migrations
+{
+    public partial class UpdateDatabaseStructure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Header",
+                table: "Projects",
+                newName: "Title");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Projects",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Projects",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Projects",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Projects");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Projects",
+                newName: "Header");
+        }
+    }
+}
