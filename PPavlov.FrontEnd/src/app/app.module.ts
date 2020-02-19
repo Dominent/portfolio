65a15ef4a4d7b3c7f5de938897c2674089fdbb15
Petromil-Pavlov <petromil-pavlov@TIDE-SOF-L0021.local>
2020-02-19 11:07:37 +0200

    Add material, implement side nav

--- PPavlov.FrontEnd/src/app/app.module.ts
+++ PPavlov.FrontEnd/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@material';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- PPavlov.FrontEnd/src/app/pages/home/home.component.ts
+++ PPavlov.FrontEnd/src/app/pages/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  public isExpanded: boolean;
 
-  constructor() { }
+  @ViewChild(MatDrawer, { static: false })
+  public drawer: MatDrawer;
 
-  ngOnInit() {
+  public onMouserEnter($event: MouseEvent) {
+    this.isExpanded = true;
   }
 
+  public onMouseLeave($event: MouseEvent) {
+    this.isExpanded = false;
+  }
 }
